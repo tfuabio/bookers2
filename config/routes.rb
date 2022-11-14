1a7e55265e32b52856cc57ab2db99f4f9f81ed27
tfuabio <114894082+tfuabio@users.noreply.github.com>
2022-11-14 06:59:35 +0000

    [update] ルーティングにresources記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/edit'
   devise_for :users
   root to: "homes#top"
   get 'homes/about' => 'homes#about', as: 'about'
   resources :books, only: [:new, :index, :show, :edit]
+  resources :users, only: [:show, :edit]
 end
