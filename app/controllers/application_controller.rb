dde81b22a071939e4ab7618215704da82809257e
tfuabio <114894082+tfuabio@users.noreply.github.com>
2022-11-15 13:51:06 +0000

    [update] フラッシュメッセージ（ユーザ新規登録以外OK）

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,28 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+
+  # サインアップ後
+  def after_sign_up_path_for(resource)
+    flash[:notice] = "Welcome! You have signed up successfully."
+    user_path(current_user.id)
+  end
+
+  # サインイン後
+  def after_sign_in_path_for(resource)
+    flash[:notice] = "Signed in successfully."
+    user_path(current_user.id)
+  end
+
+  # サインアウト後
+  def after_sign_out_path_for(resource)
+    flash[:notice] = "Signed out successfully."
+    root_path
+  end
+
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])
   end
 end
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,6 +9,7 @@ class BooksController < ApplicationController
     book = Book.new(book_params)
     book.user_id = current_user.id
     book.save
+    flash[:notice] = "You have created book successfully."
     redirect_to book_path(book.id)
   end
 
@@ -24,12 +25,14 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(book_params)
+    flash[:notice] = "You have updated book successfully."
     redirect_to book_path(book.id)
   end
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
+    flash[:notice] = "You have destroyed book successfully."
     redirect_to books_path
   end
 
