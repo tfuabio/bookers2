3cb71b53a15aacd77d94173af45d34b725568802
tfuabio <114894082+tfuabio@users.noreply.github.com>
2022-11-15 08:30:15 +0000

    [update] bookのeditページ

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   get 'homes/about' => 'homes#about', as: 'about'
-  resources :books, only: [:index, :show, :edit, :create]
+  resources :books, only: [:index, :create, :show, :edit]
   resources :users, only: [:show, :edit, :index]
 end
