8f80c863910e356e988fdbd5c87c3fdf9bd23e5f
tfuabio <114894082+tfuabio@users.noreply.github.com>
2022-11-15 10:07:55 +0000

    [update] userのupdate機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+    @book = Book.new
+  end
+
   def show
     @user = User.find(params[:id])
     @book = Book.new
@@ -8,8 +13,14 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  def index
-    @users = User.all
-    @book = Book.new
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to user_path(user.id)
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
 end
