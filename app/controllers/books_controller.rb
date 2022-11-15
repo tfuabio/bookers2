d8507a46c95158f8b7da924516b555158df2775d
tfuabio <114894082+tfuabio@users.noreply.github.com>
2022-11-15 17:00:30 +0000

    [update] bookのtitle, bodyのバリデーション

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,19 @@
 class BooksController < ApplicationController
   def index
-    @user = current_user
     @book = Book.new
     @books = Book.all
   end
 
   def create
-    book = Book.new(book_params)
-    book.user_id = current_user.id
-    book.save
-    flash[:notice] = "You have created book successfully."
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def show
