58b04ca0e5a1c04ef13eb8f56c6293c460213db6
tfuabio <114894082+tfuabio@users.noreply.github.com>
2022-11-15 06:53:00 +0000

    [update] 投稿詳細ページ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,27 @@
 class BooksController < ApplicationController
-  def new
-  end
-
   def index
     @user = current_user
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.user_id = current_user.id
+    book.save
+    redirect_to book_path(book.id)
   end
 
   def show
+    @book = Book.find(params[:id])
+    @book_new = Book.new
   end
 
   def edit
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
