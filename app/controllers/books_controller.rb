f865f852c572f5c93b7457c771d2199409abefd9
tfuabio <114894082+tfuabio@users.noreply.github.com>
2022-11-16 06:25:36 +0000

    [Fix] RSpec導入、アイコン修正、アクセス制限のリダイレクト先修正、topページのリンク修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -50,8 +50,8 @@ class BooksController < ApplicationController
   end
 
   def is_matching_login_user
-    if params[:id].to_i != current_user.id
-      redirect_to user_path(current_user.id)
+    if Book.find(params[:id]).user.id != current_user.id
+      redirect_to books_path
     end
   end
 end
